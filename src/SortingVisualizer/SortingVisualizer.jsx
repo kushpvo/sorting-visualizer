75646d3e6b1dc24ca9ca3152139f8c82f6cf6c5a
Kushan Vora <kushpvo@gmail.com>
2019-12-08 20:33:25 +0530

    Changed values

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -30,7 +30,7 @@ export default class SortingVisualizer extends React.Component {
   resetArray() {
     const array = [];
     for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randomIntFromInterval(5, 850));
+      array.push(randomIntFromInterval(5, 700));
     }
     this.setState({ array });
   }
